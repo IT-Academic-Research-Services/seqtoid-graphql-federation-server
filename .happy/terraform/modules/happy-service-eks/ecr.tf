381d2bf7d526cdc81bc5c89022a43f7ff9a3a55e
NGrande-SAI <ngrande@slower.ai>
2025-04-14 11:39:27 -0400

    Use HTTP github source for ECR module

--- .happy/terraform/modules/happy-service-eks/ecr.tf
+++ .happy/terraform/modules/happy-service-eks/ecr.tf
@@ -1,5 +1,5 @@
 module "ecr" {
-  source = "git@github.com:chanzuckerberg/cztack//aws-ecr-repo?ref=v0.59.0"
+  source = "github.com/chanzuckerberg/cztack//aws-ecr-repo?ref=v0.59.0"
 
   name           = "${var.stack_name}/${local.tags.env}/${var.container_name}"
   force_delete   = true
